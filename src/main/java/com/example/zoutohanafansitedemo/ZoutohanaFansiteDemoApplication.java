01d312c89c4952c4896edcafe26b0a369f39ca89
KazuyaNagaki <k.nagaki.sys23@morijyobi.ac.jp>
2025-11-11 13:50:14 +0900

    first commit

---
+++ src/main/java/com/example/zoutohanafansitedemo/ZoutohanaFansiteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.zoutohanafansitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoutohanaFansiteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoutohanaFansiteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zoutohanafansitedemo/ZoutohanaFansiteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zoutohanafansitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoutohanaFansiteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
